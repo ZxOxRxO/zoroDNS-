86b3e720aee1a69e6f0723ca2b0281d70d267971
Alio Gh <alio@Alios-MacBook-Air.local>
2024-07-21 20:40:41 +0330

    chanage names to zorodns as a fun :)

--- zorodns.go
+++ zorodns.go
@@ -1,13 +1,12 @@
 // Package example is a CoreDNS plugin that prints "example" to stdout on every packet received.
 //
 // It serves as an example CoreDNS plugin with numerous code comments.
-package example
+package zorodns
 
 import (
 	"context"
 
 	"github.com/coredns/coredns/plugin"
-	"github.com/coredns/coredns/plugin/metrics"
 	clog "github.com/coredns/coredns/plugin/pkg/log"
 
 	"github.com/miekg/dns"
@@ -15,16 +14,16 @@ import (
 
 // Define log to be a logger with the plugin name in it. This way we can just use log.Info and
 // friends to log.
-var log = clog.NewWithPlugin("example")
+var log = clog.NewWithPlugin("zorodns")
 
 // Example is an example plugin to show how to write a plugin.
-type Example struct {
+type Zorodns struct {
 	Next plugin.Handler
 }
 
 // ServeDNS implements the plugin.Handler interface. This method gets called when example is used
 // in a Server.
-func (e Example) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
+func (e Zorodns) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
 	// This function could be simpler. I.e. just fmt.Println("example") here, but we want to show
 	// a slightly more complex example as to make this more interesting.
 	// Here we wrap the dns.ResponseWriter in a new ResponseWriter and call the next plugin, when the
@@ -37,14 +36,14 @@ func (e Example) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg)
 	pw := NewResponsePrinter(w)
 
 	// Export metric with the server label set to the current server handling the request.
-	requestCount.WithLabelValues(metrics.WithServer(ctx)).Inc()
+	// requestCount.WithLabelValues(metrics.WithServer(ctx)).Inc()
 
 	// Call next plugin (if any).
 	return plugin.NextOrFailure(e.Name(), e.Next, ctx, pw, r)
 }
 
 // Name implements the Handler interface.
-func (e Example) Name() string { return "example" }
+func (e Zorodns) Name() string { return "zorodns" }
 
 // ResponsePrinter wrap a dns.ResponseWriter and will write example to standard output when WriteMsg is called.
 type ResponsePrinter struct {
@@ -58,6 +57,6 @@ func NewResponsePrinter(w dns.ResponseWriter) *ResponsePrinter {
 
 // WriteMsg calls the underlying ResponseWriter's WriteMsg method and prints "example" to standard output.
 func (r *ResponsePrinter) WriteMsg(res *dns.Msg) error {
-	log.Info("example")
+	log.Info("zorodns")
 	return r.ResponseWriter.WriteMsg(res)
 }
